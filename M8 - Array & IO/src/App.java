22ea7e89558ca29e319ceea3018d2ac60329a7b2
Redi Ahmad Supriyatna <ahmadredi67@gmail.com>
2022-11-14 15:04:33 +0700

    Adds a new class and method to get student score

--- M8 - Array & IO/src/App.java
+++ M8 - Array & IO/src/App.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        NilaiMahasiswa redi = new NilaiMahasiswa("Redi Ahmad", "2KB05", 2);
+        redi.inputNilai();
+        System.out.printf("Daftar nilai %s: %s", redi.nama, Arrays.toString(redi.daftarNilai));
+    }
+}
+
+class NilaiMahasiswa {
+    double[] daftarNilai;
+    String nama, kelas;
+
+    NilaiMahasiswa(String nama, String kelas, int banyakNilai) {
+        this.nama = nama;
+        this.kelas = kelas;
+
+        daftarNilai = new double[banyakNilai];
+        System.out.printf("=== Nilai Mahasiswa %s ===\n", this.nama);
+    }
+
+    public void inputNilai() {
+        double temporaryValue;
+        try (Scanner input = new Scanner(System.in)) {
+            for (int i = 0; i < daftarNilai.length; i++) {
+                System.out.print("Masukkan nilai: ");
+                temporaryValue = input.nextDouble();
+                daftarNilai[i] = temporaryValue;
+            }
+        } catch (Exception err) {
+            System.err.println("Error saat menambahkan nilai :( " + err);
+        }
     }
 }
